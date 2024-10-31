de316e7a0f61e7bf789afe5511047acf65c9f20c
D3brix <german.tammekunn@gmail.com>
2024-10-31 19:01:20 +0200

    summary
    made the board green, made food place on board and red

--- snake/snake.js
+++ snake/snake.js
@@ -14,10 +14,35 @@ var snakeBody = []
 ;
 
 ;
+var foodX
+var foodY
+;
+
+;
 window.onload = function() {
     board = document.getElementById("board")
     board.height = rows * blockSize
     board.width = cols * blockSize
     context = board.getContext("2d")
-    
+
+    placeFood()
+    document.addEventListener("keyup", changeDirection)
+    setInterval(update, 100)
+}
+
+function update() {
+    if (gameover) {
+        return
+    }
+
+    context.fillStyle="chartreuse"
+    context.fillRect(0, 0, board.width, board.height)
+
+    context.fillStyle="red"
+    context.fillRect(foodX, foodY, blockSize, blockSize)
+
+    if(snakeX == foodX && snakeY == foodY) {
+        snakeBody.push([foodX, foodY])
+        placeFood()
+    }
 }
